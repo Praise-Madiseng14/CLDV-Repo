ce350172c7aef86a6b3c1e35d94a4daf5d0c2150
Praise-Madiseng14 <praisemadiseng14@gmail.com>
2026-08-14 10:38:38 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLDV_Tutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("This is a C# console application");
+            Console.WriteLine("Running on .NET 10.0 with C# 14.0 features");
+        }
+    }
+}
